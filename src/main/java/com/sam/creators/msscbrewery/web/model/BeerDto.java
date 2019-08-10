1ed311cf587a95d0e358ee1a4b626761904f3076
sangeles <stampaa@outlook.com>
2019-08-10 10:34:52 -0500

    Data added.

---
+++ src/main/java/com/sam/creators/msscbrewery/web/model/BeerDto.java
@@ -0,0 +1,23 @@
+package com.sam.creators.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * @version 1.0.0 10/08/2019
+ * @author sangeles
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+}
